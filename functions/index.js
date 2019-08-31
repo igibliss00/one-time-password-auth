d30476d36ad5586590c67b9778223c0d9f380229
J <igibliss00@gmail.com>
2019-08-31 02:12:17 -0400

    Added create_user and twilio

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
+const admin = require("firebase-admin");
 const functions = require('firebase-functions');
+const createUser = require('./create_user')
+const serviceAccount = require('./service_account.json')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-bb6dd.firebaseio.com"
 });
+
+exports.createUser = functions.https.onRequest(createUser)
