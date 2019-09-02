109a5845d20f6f81e9b8c1f4ce48e2779c6fa295
Absence of Nunchi <38163337+igibliss00@users.noreply.github.com>
2019-09-02 00:03:04 -0400

    Update twilio.js

--- functions/twilio.js
+++ functions/twilio.js
@@ -1,6 +1,6 @@
 const twilio = require('twilio')
 
-const accountSid = 'ACaa60d4712fb527953b1d58c6a9e1bdc4'
-const authToken = 'ae697e9c10f59d7412bd204182134fc4'
+const accountSid = YOUR_SID
+const authToken = YOUR_TOKEN
 
 module.exports = new twilio.Twilio(accountSid, authToken)
